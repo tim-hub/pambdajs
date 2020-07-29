0e7fcb2e3332f29ad3ed1ea8dd3ed1b699b93b4e
tim-hub <tim_w@outlook.com>
2020-07-29 22:16:24 +1200

    more test for slice

--- src/utils/slice.spec.ts
+++ src/utils/slice.spec.ts
@@ -1,11 +1,10 @@
 import { slice } from './slice'
 
-describe('test utils', () => {
-
+describe('test slice', () => {
   const data1 = ['a', 'b', 'c', 'D', 'e']
   const count = 4
-  it('test the orchestrate', async () => {
 
+  it('5 elements', async () => {
     const r = slice(data1, count)
     expect(r).toEqual(
       [
@@ -15,6 +14,24 @@ describe('test utils', () => {
         ['D', 'e']
       ]
     )
+  })
 
+  it('slice with only 2 elements ', async () => {
+    const r = slice(data1.slice(0, 2), count)
+    expect(r).toEqual(
+      [
+        ['a'],
+        ['b']
+      ]
+    )
+  })
+  it('slice to 2 parts ', async () => {
+    const r = slice(data1, 2)
+    expect(r).toEqual(
+      [
+        ['a', 'b'],
+        ['c', 'D', 'e']
+      ]
+    )
   })
 })
